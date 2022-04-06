3ca0e8e197dc63d9319d79ce8c92a8012e8892ee
yfaimisaka <b21120429@njupt.edu.cn>
2022-04-06 11:52:42 +0800

    use recursive to solve insert_sort

--- sort/insert_sort.cpp
+++ sort/insert_sort.cpp
@@ -25,12 +25,34 @@ vector<int> insert_sort(vector<int>& A)
     return A;
 }
 
+// use recursive to relalize insert_sort
+void insert_sort_recursive(vector<int>& A, int length)
+{
+    if (length > 1)
+    {
+        insert_sort_recursive(A, length - 1);
+        // use i while not length - 2 directly
+        int i = length - 2;
+        // as need to differ the key and the 
+        // vector that has been sorted.
+
+        // also save a copy to to escape changing its value
+        int key = A[length - 1];
+        while (i >= 0 && A[i] > key)
+        {
+            A[i + 1] = A[i];
+            --i;
+        }
+        A[i + 1] = key;
+    }
+}
 
 // TEST
 // int main()
 // {
 //     vector<int> test = {3, 2, 5, 1};
-//     for (int a : insert_sort(test))
+//     // or: insert_sort_recursive(test, test.size())
+//     for (int a : insert_sort(test))  
 //     {
 //         cout << a << "\n";
 //     }
